26eda6cb59594e80a6c16354040c9267e006b73e
Enrique Vergara <enrique.vergara@atreva.mx>
2018-10-02 08:48:12 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MzNavbarModule } from 'ngx-materialize';
+import { MzParallaxModule } from 'ngx-materialize';
+import { MzInputModule } from 'ngx-materialize';
+import { MzIconModule, MzIconMdiModule } from 'ngx-materialize';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MzNavbarModule,
+    MzParallaxModule,
+    MzInputModule,
+    MzIconModule,
+    MzIconMdiModule
   ],
   providers: [],
   bootstrap: [AppComponent]
